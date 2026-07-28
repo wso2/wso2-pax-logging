b3cbd9cba7e70ae5d4b69398ecd7862de80da00c
Saadha123 <saadha.salim1@gmail.com>
2026-07-28 10:15:43 +0530

    Upgrade to Log4j2 2.26.0
    Cherry-picked from ops4j/org.ops4j.pax.logging commit eb192ff5
    (pax-logging-2.2.x branch). Fixes CVE-2026-49844.

--- pax-logging-log4j2/src/main/java/org/apache/logging/log4j/core/appender/db/jdbc/JdbcDatabaseManager.java
+++ pax-logging-log4j2/src/main/java/org/apache/logging/log4j/core/appender/db/jdbc/JdbcDatabaseManager.java
@@ -50,6 +50,7 @@ import org.apache.logging.log4j.core.appender.db.ColumnMapping;
 import org.apache.logging.log4j.core.appender.db.DbAppenderLoggingException;
 import org.apache.logging.log4j.core.config.plugins.convert.DateTypeConverter;
 import org.apache.logging.log4j.core.config.plugins.convert.TypeConverters;
+import org.apache.logging.log4j.core.internal.annotation.SuppressFBWarnings;
 import org.apache.logging.log4j.core.util.Closer;
 import org.apache.logging.log4j.core.util.Log4jThread;
 import org.apache.logging.log4j.message.MapMessage;
@@ -711,6 +712,9 @@ public final class JdbcDatabaseManager extends AbstractDatabaseManager {
         return true;
     }
 
+    @SuppressFBWarnings(
+            value = "SQL_INJECTION_JDBC",
+            justification = "The SQL statement is generated based on the configuration file.")
     private void connectAndPrepare() throws SQLException {
         logger().debug("Acquiring JDBC connection from {}", this.getConnectionSource());
         this.connection = getConnectionSource().getConnection();
@@ -786,6 +790,9 @@ public final class JdbcDatabaseManager extends AbstractDatabaseManager {
         return factoryData.tableName;
     }
 
+    @SuppressFBWarnings(
+            value = "SQL_INJECTION_JDBC",
+            justification = "The SQL statement is generated based on the configuration file.")
     private void initColumnMetaData() throws SQLException {
         // Could use:
         // this.connection.getMetaData().getColumns(catalog, schemaPattern, tableNamePattern, columnNamePattern);
